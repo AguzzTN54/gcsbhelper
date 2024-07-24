1ddc866c38a789a54baa168df74ba6688853f935
AguzzTN54 <gusagusnanang@gmail.com>
2024-07-24 17:36:08 +0700

    Arcade

--- src/routes/arcade/+page.svelte
+++ src/routes/arcade/+page.svelte
@@ -1,11 +1,64 @@
 <script>
-	import Arcade from './_index.svelte';
+	import { formatTime, arcadeDate, timeZone } from '$lib/helpers/dateTime';
+	import { profile } from '$lib/stores/app-store';
+	import Summary from './Summary.svelte';
+	import UrlForm from './_URLForm.svelte';
+	import Details from './Details.svelte';
+
+	$: ({ user, points } = $profile);
+	const { end, start } = arcadeDate;
+	const startDate = formatTime(start);
+	const endDate = formatTime(end);
 </script>
 
-<Arcade />
+<section class="hero">
+	<div class="top">
+		<h1>Arcade Point Counter</h1>
+		<span> {startDate} - {endDate} ({timeZone})</span>
+	</div>
+
+	<div class="info-container">
+		{#if !!user}
+			<Summary {user} {points} />
+		{:else}
+			<UrlForm />
+		{/if}
+	</div>
+</section>
+
+{#if user}
+	<Details />
+{/if}
 
 <style>
-	/* .dark {
-		background-color: #202124;
-	} */
+	.hero {
+		width: var(--screen-width);
+		min-height: var(--screen-height);
+		text-align: center;
+		padding: 2% 5%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: xx-large;
+	}
+
+	.top {
+		margin-top: 5%;
+		margin-bottom: auto;
+	}
+
+	.info-container {
+		width: 100%;
+		height: calc(0.7 * var(--screen-height));
+		margin-bottom: auto;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+	}
 </style>
