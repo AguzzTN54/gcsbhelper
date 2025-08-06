d5759194fc673f5d92d1626f6ab8d2f0aa0ae9ac
AguzzTN54 <gusagusnanang@gmail.com>
2025-08-06 23:02:07 +0700

    chore: dynamic notification icon

--- client/static/sw.js
+++ client/static/sw.js
@@ -1,11 +1,12 @@
 self.addEventListener('push', (event) => {
-	const { url, body, icon, title } = event.data?.json() ?? {};
+	const { url, body, icon: icn, title } = event.data?.json() ?? {};
 	const cdn = 'https://imagecdn.wishsimulator.app/cb';
+	const icon = icn.id && icn.slug ? `${cdn}/${icn.id}/${icn.slug}&w=200&h=200&fit=cover` : icn.href;
 	event.waitUntil(
 		self.registration.showNotification(title || 'Notification', {
 			data: url,
 			body,
-			icon: `${cdn}/${icon.id}/${icon.slug}&w=200&h=200&fit=cover`,
+			icon,
 			image: cdn + '/nqzqyrt3/GC-arcade-header-diwali-9.png&w=512&h=256&fit=cover',
 			badge: 'https://cdn.qwiklabs.com/X46FrQX4iLxHW5MxL8jICvgZM0evMEKscCeQO%2BazGdo%3D',
 			vibrate: [200, 100, 200],
