b2a9ccda8847fdefcdbc990c24189d7d3984ddc4
AguzzTN54 <gusagusnanang@gmail.com>
2025-08-11 07:28:56 +0700

    feat: Update dependency

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,37 +1,38 @@
 import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
+import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import svelteConfig from './svelte.config.js';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
+	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
-	...svelte.configs['flat/recommended'],
+	...svelte.configs.recommended,
 	prettier,
-	...svelte.configs['flat/prettier'],
+	...svelte.configs.prettier,
+	...pluginQuery.configs['flat/recommended'],
 	{
 		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-				App
-			}
-		}
+			globals: { ...globals.browser, ...globals.node }
+		},
+		rules: { 'no-undef': 'off', 'svelte/no-at-html-tags': 'off' }
 	},
 	{
-		files: ['**/*.svelte'],
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		languageOptions: {
 			parserOptions: {
-				parser: ts.parser
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
+				parser: ts.parser,
+				svelteConfig
 			}
-		}
-	},
-	{
-		ignores: ['build/', '.svelte-kit/', 'dist/'],
-		rules: {
-			'svelte/no-at-html-tags': 'off',
-			'@typescript-eslint/ban-ts-comment': 'off'
 		}
 	}
 );
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vite';
-import { plugin as MdPlugin } from 'vite-plugin-markdown';
 
 export default defineConfig({
-	// @ts-ignore
-	plugins: [sveltekit(), MdPlugin({ mode: 'markdown' })],
+	plugins: [sveltekit(), tailwindcss(), devtoolsJson()],
 	build: {
 		chunkSizeWarningLimit: 350,
 		target: ['es2020']
