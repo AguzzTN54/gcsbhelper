14d40693a63adfe66cf2d4b078b83210f06cf03f
AguzzTN54 <gusagusnanang@gmail.com>
2025-08-23 22:28:56 +0700

    feat: Profile Resolver Request

--- client/src/routes/_global/reusable/Button.svelte
+++ client/src/routes/_global/reusable/Button.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-	const { children, onclick: click = null } = $props();
+	const { children, onclick: click = null, disabled = false } = $props();
 	const onclick = () => click?.();
 </script>
 
 <button
 	{onclick}
+	{disabled}
+	class:disabled
 	class="bg-amber-300 hover:bg-amber-400/80 px-10 py-2 text-lg mt-5 brutal-shadow-soft brutal-border !border-[3px] active:shadow-none rounded-xl"
 >
 	{@render children()}
 </button>
+
+<style lang="postcss">
+	@import 'tailwindcss/theme' theme(reference);
+
+	.disabled {
+		@apply opacity-40 cursor-not-allowed shadow-none;
+	}
+</style>
--- client/src/routes/arcade/_/Form.svelte
+++ client/src/routes/arcade/_/Form.svelte
@@ -2,50 +2,105 @@
 	import { goto } from '$app/navigation';
 	import { PUBLIC_API_SERVER } from '$env/static/public';
 	import { createToken } from '$lib/helpers/crypto';
-	import { uuidToHex } from '$lib/helpers/uuid';
+	import { isValidUUID, uuidToHex } from '$lib/helpers/uuid';
 	import Button from '$reusable/Button.svelte';
 	import Checkbox from '$reusable/Checkbox.svelte';
+	import Loading from '$reusable/Loading.svelte';
 
+	const isGCSBUrl = (url: string) => /cloudskillsboost.google\/public_profiles\//.test(url);
+	const validateURL = (url: string) => {
+		const pattern = new RegExp(
+			'^([a-zA-Z]+:\\/\\/)?' + // protocol
+				'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+				'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
+				'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+				'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+				'(\\#[-a-z\\d_]*)?$', // fragment locator
+			'i'
+		);
+		return pattern.test(url);
+	};
+
+	let loading = $state(false);
+	let fetchError = $state(false);
 	let value = $state('');
+	let typed = $state(false);
 	let submitGlobal = $state(true);
+	const profileUUID = $derived.by<string>(() => {
+		const profileLink = value.trim();
+		const isgcsb = isGCSBUrl(profileLink);
+		if (!isgcsb) return '';
+		if (!validateURL(profileLink)) return '';
+		try {
+			const { pathname } = new URL(profileLink);
+			const [, , uuid] = pathname.split('/');
+			if (!isValidUUID(uuid)) return '';
+			return uuid;
+		} catch {
+			return '';
+		}
+	});
 
 	const fetchProfile = async () => {
-		// const token = await createToken();
-		// const profileid = uuidToHex('eb24c815-5b47-4afb-ac14-8fc51e876c6a');
-		// const res = await fetch(PUBLIC_API_SERVER + '/api/identity/' + profileid, {
-		// 	headers: { 'x-subscribe-token': token }
-		// });
-		// const data = await res.json();
-		// console.log(data);
-		goto('/arcade/dash');
+		try {
+			loading = true;
+			const profileid = uuidToHex(profileUUID);
+			const token = await createToken();
+			const res = await fetch(PUBLIC_API_SERVER + '/api/identity/' + profileid, {
+				headers: { 'x-subscribe-token': token }
+			});
+			if (res.status !== 200) throw new Error('Fetch Error');
+
+			const data = await res.json();
+			console.log(data);
+			loading = false;
+		} catch (e) {
+			console.error(e);
+			loading = false;
+			fetchError = true;
+		}
 	};
 </script>
 
 <div class="flex w-full py-5 flex-col h-full items-center justify-center">
-	<div class="input relative rounded-full w-10/12 sm:w-[60%]">
-		<input
-			bind:value
-			type="text"
-			class="brutal-border !border-[4px] bg-gray-100 rounded-full h-16 px-5 placeholder:font-medium w-full outline-0"
-			placeholder="Input Public Profile URL"
-		/>
-		<span
-			class="absolute top-0 right-0 text-[1.75rem] md:text-[2.5rem] translate-x-2/3 -translate-y-1/2"
+	{#if loading}
+		<Loading />
+		<span class="inline-block mt-5"> Hang tight! We’re grabbing your data… </span>
+	{:else}
+		{#if fetchError}
+			<div class="text-center text-red-600">Unable to load your data. Please try again!</div>
+		{/if}
+		<div class="input relative rounded-full w-10/12 sm:w-[60%]">
+			<input
+				bind:value
+				oninput={() => {
+					typed = true;
+					fetchError = false;
+				}}
+				type="text"
+				class:!border-red-600={fetchError || (!profileUUID && typed)}
+				class:!text-red-600={fetchError || (!profileUUID && typed)}
+				class="brutal-border !border-[4px] bg-gray-100 rounded-full h-16 px-5 placeholder:font-medium w-full outline-0"
+				placeholder="Input Public Profile URL"
+			/>
+			<span
+				class="absolute top-0 right-0 text-[1.75rem] md:text-[2.5rem] translate-x-2/3 -translate-y-1/2"
+			>
+				<i class="fasdl fa-sparkles text-indigo-400"></i>
+			</span>
+		</div>
+
+		<Checkbox
+			checked={submitGlobal}
+			onchange={(e) => (submitGlobal = (e.target as HTMLInputElement).checked)}
 		>
-			<i class="fasdl fa-sparkles text-indigo-400"></i>
-		</span>
-	</div>
+			Submit to global analytics
+		</Checkbox>
 
-	<Checkbox
-		checked={submitGlobal}
-		onchange={(e) => (submitGlobal = (e.target as HTMLInputElement).checked)}
-	>
-		Submit to global analytics
-	</Checkbox>
-
-	<Button onclick={fetchProfile}>
-		Calculate Points <i class="fasdl fa-stars text-rose-300"></i>
-	</Button>
+		<Button onclick={fetchProfile} disabled={!profileUUID}>
+			Calculate Points <i class="fasdl fa-stars text-rose-300"></i>
+		</Button>
+	{/if}
 </div>
 
 <style lang="postcss">
