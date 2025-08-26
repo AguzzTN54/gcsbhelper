8dd9c1946560f3e372676c9b52b70b525607bf94
AguzzTN54 <gusagusnanang@gmail.com>
2025-08-27 03:17:06 +0700

    feat: Enddate field

---
+++ server/pocketbase/migrations/1756225055_updated_courses.js
@@ -0,0 +1,26 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_955655590")
+
+  // add field
+  collection.fields.addAt(12, new Field({
+    "hidden": false,
+    "id": "date2440358909",
+    "max": "",
+    "min": "",
+    "name": "enddate",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "date"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_955655590")
+
+  // remove field
+  collection.fields.removeById("date2440358909")
+
+  return app.save(collection)
+})
