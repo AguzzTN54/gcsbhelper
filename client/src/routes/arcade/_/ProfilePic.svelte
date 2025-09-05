898b6eccdc5686717343c3740c51a02d167db4c8
AguzzTN54 <gusagusnanang@gmail.com>
2025-09-05 17:41:28 +0700

    feat: Profile Pic Placeholder

--- client/src/routes/arcade/_/ProfilePic.svelte
+++ client/src/routes/arcade/_/ProfilePic.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { profileReady } from '$lib/stores/app-store';
 	import anonym from '$img/avatar.webp';
 	import Skeleton from '$reusable/Skeleton.svelte';
@@ -9,7 +9,12 @@
 <div class="size-20 bg-amber-300 rounded-full p-0.5">
 	<div class="size-full bg-indigo-300 rounded-full overflow-hidden object-cover p-1">
 		{#if $profileReady}
-			<img {src} alt="Profile" class="rounded-full brutal-border !border-[3px]" />
+			<img
+				{src}
+				onerror={(e) => ((e.target as HTMLImageElement).src = anonym)}
+				alt="Profile"
+				class="rounded-full brutal-border !border-[3px]"
+			/>
 		{:else}
 			<Skeleton class="size-full rounded-full" />
 		{/if}
