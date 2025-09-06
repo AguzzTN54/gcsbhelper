8d4db2a7d22e20400de31b87b18713112b883d09
AguzzTN54 <gusagusnanang@gmail.com>
2025-09-07 00:59:06 +0700

    feat: Start Date Field

--- server/deno-functions/src/lib/scrapper/courses/games.ts
+++ server/deno-functions/src/lib/scrapper/courses/games.ts
@@ -1,5 +1,5 @@
 // @deno-types="npm:@types/jsdom"
-import { JSDOM } from 'npm:jsdom';
+import { JSDOM, DOMWindow } from 'npm:jsdom';
 import { type ArcadeContent } from '../../db/denoKv.ts';
 import { shortShaId } from '../../utils/hash.ts';
 import { pb } from '../../db/pocketbase.ts';
@@ -8,10 +8,18 @@ const gcsb = 'https://www.cloudskillsboost.google';
 interface GcsbData {
   badgeid: number;
   totallab: number;
+  startdate: string | null | Date;
   enddate: string | null | Date;
   badgeurl: string;
   title: string;
 }
+
+const getDate = (window: DOMWindow, dateIndex: number = 0): string | null => {
+  const time = window.document.querySelectorAll('ql-datetime')[dateIndex];
+  const timeMS = time?.getAttribute('millisecondsSinceEpoch');
+  const date = timeMS ? new Date(parseInt(timeMS)).toISOString() : null;
+  return date;
+};
 const crawlGamePage = async (id: number): Promise<GcsbData | null> => {
   try {
     const target = gcsb + '/games/' + id;
@@ -24,17 +32,14 @@ const crawlGamePage = async (id: number): Promise<GcsbData | null> => {
     const title = window.document.querySelector('h1.ql-display-large')?.textContent || '';
     const badgeurl = window.document.querySelector('.game__badge > img')?.getAttribute('src') || '';
 
-    const time = window.document.querySelectorAll('ql-datetime')[1];
-    const timeMS = time?.getAttribute('millisecondsSinceEpoch');
-    const enddate = timeMS ? new Date(parseInt(timeMS)).toISOString() : null;
-
     const dataString = courseContent?.getAttribute('modules');
     const data = dataString ? JSON.parse(dataString) : [];
     const totallab = data
       .filter((item: { title: string }) => item.title !== "What's Next")
       .reduce((sum: number, item: { steps: string[] }) => sum + item.steps.length, 0);
-
-    return { badgeid, totallab, enddate, badgeurl, title };
+    const startdate = getDate(window, 0);
+    const enddate = getDate(window, 1);
+    return { badgeid, totallab, startdate, enddate, badgeurl, title };
   } catch (e) {
     console.error(e);
     return null;
@@ -49,12 +54,13 @@ interface Course {
   totallab?: number;
   badgeurl?: string;
   type?: string;
+  startdate?: string | Date | null;
   enddate?: string | Date | null;
   point?: number;
 }
 
 const insertToPb = async (course: Course) => {
-  const { title, totallab, badgeurl, courseid, point, type, enddate, badgeid, token } = course || {};
+  const { title, totallab, badgeurl, courseid, point, type, startdate, enddate, badgeid, token } = course || {};
   if (!title || !badgeid) return;
   try {
     const id = await shortShaId(`${badgeid}`);
@@ -65,6 +71,7 @@ const insertToPb = async (course: Course) => {
         body: {
           id,
           enddate,
+          startdate,
           courseid,
           title,
           totallab,
