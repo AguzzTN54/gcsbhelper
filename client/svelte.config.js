1c8313db11831cfb0d12e4574ae75b28137a5883
AguzzTN54 <gusagusnanang@gmail.com>
2025-09-09 03:38:48 +0700

    feat: Static Build

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
