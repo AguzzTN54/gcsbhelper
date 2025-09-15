c0e0e3bb6db66536460bf91435b90138e4a14dc8
AguzzTN54 <gusagusnanang@gmail.com>
2025-09-16 03:33:27 +0700

    feat: Pocketbase Rule

---
+++ server/pocketbase/migrations/1757959529_updated_labs.js
@@ -0,0 +1,22 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2691397795")
+
+  // update collection data
+  unmarshal({
+    "listRule": "@request.auth.email ?~ \"@ekraf.dev\" && @request.headers.x_arcade_token != \"\"",
+    "viewRule": "@request.auth.email ?~ \"@ekraf.dev\" && @request.headers.x_arcade_token != \"\""
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2691397795")
+
+  // update collection data
+  unmarshal({
+    "listRule": "",
+    "viewRule": ""
+  }, collection)
+
+  return app.save(collection)
+})
